0da5a5ece5e5dd5f7a1ca1360a14fcaa69b01b29
hhhhhq <Mr.j@tom.com>
2020-05-21 10:58:23 +0800

    Updates

---
+++ precache-manifest.9c875732108190ff938f04acbecbbb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4369e92940b12f91b459d23c333279",
+    "url": "/hello-todos/index.html"
+  },
+  {
+    "revision": "22fb0783ea247e25daef",
+    "url": "/hello-todos/static/css/main.14f8c9c1.chunk.css"
+  },
+  {
+    "revision": "933ab57e319a6b69f937",
+    "url": "/hello-todos/static/js/2.cf4fce51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hello-todos/static/js/2.cf4fce51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fb0783ea247e25daef",
+    "url": "/hello-todos/static/js/main.417bf5d0.chunk.js"
+  },
+  {
+    "revision": "44c68f84d3a5cc0a7073",
+    "url": "/hello-todos/static/js/runtime-main.453dd5f3.js"
+  }
+]);
